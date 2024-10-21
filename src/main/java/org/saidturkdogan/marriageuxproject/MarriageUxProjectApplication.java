f267befa9cd71943318b3030400573b1147125bb
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2024-10-21 23:33:58 +0300

    first

---
+++ src/main/java/org/saidturkdogan/marriageuxproject/MarriageUxProjectApplication.java
@@ -0,0 +1,13 @@
+package org.saidturkdogan.marriageuxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarriageUxProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarriageUxProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/saidturkdogan/marriageuxproject/MarriageUxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.saidturkdogan.marriageuxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarriageUxProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
