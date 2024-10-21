fdb57011a98189d3b6bc99b9e19a22c212e77ce4
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2024-10-21 23:46:08 +0300

    commit

---
+++ src/main/java/org/saidturkdogan/marriageuxproject/model/Gift.java
@@ -0,0 +1,27 @@
+package org.saidturkdogan.marriageuxproject.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+
+public class Gift {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    private GiftType giftType;
+}
---
+++ src/main/java/org/saidturkdogan/marriageuxproject/model/WeddingGuest.java
@@ -0,0 +1,26 @@
+package org.saidturkdogan.marriageuxproject.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class WeddingGuest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private Gift gift;
+
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private Guest guest;
+}
